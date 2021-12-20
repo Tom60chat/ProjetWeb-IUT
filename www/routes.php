d80d8d8bf871be61b9f65be7abac1e5f48a20e49
d-oceane <78603636+d-oceane@users.noreply.github.com>
2021-12-20 12:21:24 +0100

    ajout routes de la page candidate

--- www/routes.php
+++ www/routes.php
@@ -45,13 +45,13 @@ Flight::route('POST /register-candidate', function () {
             $messages['Email'] = "L'email est déjà enregistré.";
     }
 
-    // Verification MotsDePass
+    // Verification MotDePasse
     if (empty(trim($data->Motdepasse)))
         $messages['Motdepasse'] = "Le mots de pass est vide.";
     else if (strlen($data->Motdepasse) < 8)
         $messages['Motdepasse'] = "Le mots de pass est trop court (Minimum 8 caractères).";
 
-    // Écheque
+    // Échec
     if (count($messages) > 0) {
         $depQuery = $pdo->query('select num_departement,nom_departement from departement');
         $sceneQuery = $pdo->query('select id_scene,type_scene from scene');
@@ -113,7 +113,7 @@ Flight::route('POST /login', function () {
             $messages['Email'] = "Identifiants incorrects";
     }
 
-    // Verification MotsDePass
+    // Verification MotDePasse
     if (empty(trim($data->Motdepasse)))
         $messages['Motdepasse'] = "Le mots de pass est vide.";
     else if (strlen($data->Motdepasse) < 8)
@@ -144,11 +144,54 @@ Flight::route('POST /login', function () {
         }
     }
 
-    // Écheque
+    // Échec
     $tab = array(
         'data' => $data,
         'messages' => $messages,
     );
 
     Flight::render('templates/login.tpl', $tab);
+});
+
+Flight::route('/candidate', function(){
+    //cette page est accessible seulement à un utilisateur de type utilisateur 
+    if(!isset($_SESSION['user']) || $_SESSION['user']['type']!="utilisateur"){
+        Flight::redirect('/');
+    } 
+    else{
+        //on prépare et éxecute une requête pour obtenir les informations d'une canditure
+        $req=Flight::get('db')->prepare('SELECT * FROM candidature WHERE email_representant like :email');
+        $req->execute(array(':email'=>$_SESSION['user']['Email']));
+        //on convertit le résultat de la requête sous forme d'un tableau de chaine de caractère
+        $groupe=$req->fetch();
+        //on a stocké les liens des 3 fichiers mp3 dans un même champ, on les sépare
+        $tab3=explode(";",$groupe['fichiersmp3']);
+        $groupe['fichiersmp3']=$tab3;
+        //on a stocké les liens des 2 images dans un même champ, on les sépare
+        $tab2=explode(";",$groupe['photos']);
+        $groupe['photos']=$tab2;
+        Flight::render('templates/candidate.tpl', array('groupe'=>$groupe,'session'=>$_SESSION['user']));
+    }
+});
+
+Flight::route('/candidate-@idgroupe', function($idgroupe){
+    //l'admin provient de la page de liste des candidatures, on utilise le num_groupe en paramètre dans le lien pour savoir le groupe dont on veut afficher la candidature
+    //cette page est accessible seulement à un utilisateur de type admin
+    if(!isset($_SESSION['user']) || $_SESSION['user']['type']!="admin"){
+        Flight::redirect('/');
+    } 
+    else{
+        //on prépare et éxecute une requête pour obtenir les informations d'une candidature
+        $req=Flight::get('db')->prepare('SELECT * FROM candidature WHERE num_groupe like :id');
+        $req->execute(array(':id'=>$idgroupe));
+        //on convertit le résultat de la requête sous forme d'un tableau de chaine de caractère
+        $groupe=$req->fetch();
+        //on a stocké les liens des 3 fichiers mp3 dans un même champ, on les sépare
+        $tab3=explode(";",$groupe['fichiersmp3']);
+        $groupe['fichiersmp3']=$tab3;
+        //on a stocké les liens des 2 images dans un même champ, on les sépare
+        $tab2=explode(";",$groupe['photos']);
+        $groupe['photos']=$tab2;
+        Flight::render('templates/candidate.tpl', array('groupe'=>$groupe,'session'=>$_SESSION['user']));
+    }
 });
